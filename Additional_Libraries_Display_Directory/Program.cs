c803382dd2647544ccdfd92b03dfa0e48f0eb9e8
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2019-04-21 21:26:51 +0200

    Add project files.

---
+++ Additional_Libraries_Display_Directory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Additional_Libraries_Display_Directory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
